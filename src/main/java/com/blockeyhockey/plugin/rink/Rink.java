7a34e9399ac39844cf13e4f0ac0fb70357ec3aa9
Jake <66141166+harvanchik@users.noreply.github.com>
2021-02-01 21:18:10 -0800

    added hockey player list for all players belonging to the rink

--- src/main/java/com/blockeyhockey/plugin/rink/Rink.java
+++ src/main/java/com/blockeyhockey/plugin/rink/Rink.java
@@ -2,7 +2,10 @@ package com.blockeyhockey.plugin.rink;
 
 import com.blockeyhockey.plugin.BlockeyHockey;
 import com.blockeyhockey.plugin.game.HockeyGameManager;
+import com.blockeyhockey.plugin.player.HockeyPlayer;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
 
 /**
  * This object holds information about a hockey rink.
@@ -14,10 +17,17 @@ public class Rink {
 
     private final BlockeyHockey plugin;
 
-    private final RinkDimension rinkDimension;
+    private final RinkDimension rinkDimension;          // the rink's dimensions
 
-    private final HockeyGameManager gameManager;
+    private final HockeyGameManager gameManager;        // the rink's game manager
 
+    private final ArrayList<HockeyPlayer> hockeyPlayers = new ArrayList<>();
+
+    /**
+     * Create a hockey rink.
+     * @param plugin The plugin's main instance.
+     * @param rinkDimension The dimensions for the rink.
+     */
     public Rink(@NotNull final BlockeyHockey plugin, @NotNull final RinkDimension rinkDimension) {
         this.plugin = plugin;
         this.rinkDimension = rinkDimension;
